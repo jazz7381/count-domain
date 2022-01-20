a449cec9905d235fbbd1ce1719331d31fa52e68d
rokumens <7381@asiapowergames.com>
2022-01-20 19:03:54 +0700

    init

---
+++ index.php
@@ -0,0 +1,36 @@
+#!/usr/local/bin/php
+<?php
+
+require 'vendor/autoload.php';
+use Rap2hpoutre\FastExcel\FastExcel;
+
+$filePath = $argv[1];;
+$collections = (new FastExcel)->import($filePath);
+$domains = [];
+
+foreach ($collections as $collection) {
+
+    $arrayValue = array_values($collection);
+    
+    $phone = $arrayValue[0];
+    $domain = $arrayValue[1];
+
+    if (isset($domains[$domain])) {
+        $domains[$domain]++;
+    } else {
+        $domains[$domain] = 1;
+    }
+}
+
+$exportFileName = 'result-' . $filePath;
+
+$fp = fopen($exportFileName, 'w');
+foreach ($domains as $key => $fields) {
+    fputcsv($fp, [
+        $key,
+        $fields
+    ]);
+}
+fclose($fp);
+
+dd("Completed : $exportFileName");
